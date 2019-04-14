2c2fb2c474d3447523a66da05fd0d00831f3205f
Jarred Deeley <jarred.deeley@gmail.com>
2019-04-14 01:54:32 -0500

    game_logic: Merged the Scripts and scripts folders

--- Assets/Scripts/game_logic/scoring_rules.cs
+++ Assets/Scripts/game_logic/scoring_rules.cs
@@ -6,13 +6,15 @@ public class scoring_rules : MonoBehaviour
 {
     public GameObject attacker;
 
-    public GameObject player_1_area;
-    public GameObject player_2_area;
+    public bool player_1_area_collided;
+    public bool player_2_area_collided;
     public GameObject out_of_bounds_area;
 
     public Score score;
 
     private int ball_bounce_count;
+    private string player_1_name = "player1";
+    private string player_2_name = "player2";
     
     // Start is called before the first frame update
     void Start()
@@ -32,27 +34,47 @@ public class scoring_rules : MonoBehaviour
     }
 
     // ball hits player area
-    public void ball_collided_player_area(GameObject collided_area) {
+    public void ball_collided_player_area() {
+        bool ball_hit_defender_side = false;
+
+        if (player_1_area_collided && attacker.name.Equals(player_2_name)) {
+            ball_hit_defender_side = true;
+        }
+        else if (player_2_area_collided && attacker.name.Equals(player_1_name)) {
+            ball_hit_defender_side = true;
+        }
+            
         //area is not attacker's
-        if (true) {
+        if (ball_hit_defender_side) {
             ball_bounce_count++;
             if (ball_bounce_count > 1) {
-                //POINT for attacker
                 score.increment_player_score(attacker.name);
                 // attacker serves
             }
         } else {
-            // POINT for defender
+            // POINT! for defender
+            give_point_to_defender();
         }   
     }
 
     public void ball_collided_out_of_bounds() {
         // ignoring possible edge case of out of bounds happening on the attacker's side
         if (ball_bounce_count > 1) {
-            //POINT for attacker
+            score.increment_player_score(attacker.name);
         } else {
             //POINT for defender
+            give_point_to_defender();
+        }
+    }
+
+    private void give_point_to_defender() {
+        if (attacker.name.Equals(player_1_name)) {
+            score.increment_player_score(player_2_name);
+        }
+        else {
+            score.increment_player_score(player_1_name);
         }
     }
 
 }
+
