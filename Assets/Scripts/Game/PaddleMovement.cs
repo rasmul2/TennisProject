e37589aacdeb9751f693ad65222c2ad739321f16
Isaac Buitrago <mightyking7@gmail.com>
2019-04-13 22:19:58 -0500

    Resetting branch to last working commit

---
+++ Assets/Scripts/Game/PaddleMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controller to move a player's paddle and hit the ball.
+/// <Author>Isaac Buitrago</Author>
+/// </summary>
+public class PaddleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.
+        
+    }
+}
